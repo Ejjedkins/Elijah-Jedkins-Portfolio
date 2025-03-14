1a3b714a5d6c57725d70f5b612bb7ae4055aa961
Ejjedkins <127382824+Ejjedkins@users.noreply.github.com>
2025-03-14 16:26:26 -0500

    Added functionality to Hamburger Menu w/ toggleMenu fuction

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Defining toggle Menu */
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
